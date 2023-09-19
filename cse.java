8530af19703ff201c0e1017ffc0f9f732d9cccae
palanivel <palanivel.21ucs@mahendra.info>
2023-09-19 12:36:27 +0530

    version 4

--- cse.java
+++ cse.java
@@ -1,7 +1,11 @@
 class main{
     public static void main(String[]args)
-    {
+    { 
+        String a="palani";
         System.out.println("nothing say");
         System.out.println("github");
           System.out.println("mano");
+          System.out.println("palani");
+          System.out.println("sathi");
+      
     }
